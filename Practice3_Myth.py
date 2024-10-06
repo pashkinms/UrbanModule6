570ad97e49f191a960b6f988633a9fede63b8bf0
AlcheMystic <pashkinms86@gmail.com>
2024-10-07 06:45:50 +0700

    Task3 Complete

---
+++ Practice3_Myth.py
@@ -0,0 +1,41 @@
+class Horse:
+    x_distance = 0
+    sound = "Frrr"
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+
+    y_distance = 0
+    sound = 'I train, eat, sleep and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Eagle, Horse):
+
+    def move(self, dx, dy):
+        self.x_distance += dx
+        self.y_distance += dy
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(super().sound)
+
+
+print(Pegasus.mro())
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
